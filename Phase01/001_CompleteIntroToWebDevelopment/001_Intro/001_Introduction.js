29d2731b33d2164ba6469120ee7f51c1c6f5a5b5
Anthony Lewallen <lewallenae@gmail.com>
2026-05-24 20:48:33 -0700

    Initializing the repo

---
+++ Phase01/001_CompleteIntroToWebDevelopment/001_Intro/001_Introduction.js
@@ -0,0 +1,10 @@
+// This course is to learn the fundamentals of coding.
+
+
+// It culminates into creating a wordle clone. A program that sold for millions of dollars.
+
+// You will have enough knowledge after this course to create a similar program, but you might not sell it for millions of dollars.
+
+// As taught by Brian Holt.
+
+// And Expect to hit a wall multiple times throughout the course. You WILL be pushed hard....very hard.
---
+++ Phase01/001_CompleteIntroToWebDevelopment/001_Intro/002_Tools.js
@@ -0,0 +1,9 @@
+// Use VSCode it is the most flexible for the full-stack
+
+// For any resources use the Mozilla MDN documents. HTML, CSS, JavaScript etc. Very technical info
+
+// CSS Tricks is a good resource for tutorials.
+
+// If using a library or framework head to the GitHub
+
+// Look at the FrontEnd Handbook by FEM.
